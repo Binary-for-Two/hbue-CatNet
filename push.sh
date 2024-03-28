5c7f7a6c8e14f6ff5051035a17548c26752eea45
William Colton <forfun.william.c@gmail.com>
2024-03-29 01:08:29 +0800

    update

--- push.sh
+++ push.sh
@@ -2,3 +2,5 @@ git add * >> /home/williamcolton/Work/Web/hbue-CatNet/log.txt
 git commit -m "update" >> /home/williamcolton/Work/Web/hbue-CatNet/log.txt
 git push >> /home/williamcolton/Work/Web/hbue-CatNet/log.txt
 
+
+
